38f443014c85b01bbf94ecf17904c9aa1d33afa3
Chris Lyko <chrislyko@gmail.com>
2020-12-23 09:26:58 -0500

    Fixed seeding error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,7 @@
+USE burgers_db;
+
 INSERT INTO burgers (burger_name, devoured)
 VALUES ("Baconator", false), 
 ("Hamburger", true),
 ("McDouble", false),
-("Bigboy Burger", true)
+("Bigboy Burger", true);
