561b01d11085d049d757e27c2f19b53ca7bb0677
Chris Lyko <chrislyko@gmail.com>
2020-12-30 12:19:06 -0500

    filled oput orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,32 @@
 const connection = require("../config/connection.js");
 
 const orm = {
-    selectAll: () => {},
-    insertOne: () => {},
-    updateOne: () => {}
+    selectAll: (input, cb) => {
+        connection.query(query, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne: (table, value, cb) => {
+        const query = "INSERT INTO " + table + " SET ?";
+        connection.query(query, value, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    updateOne: (table, condition, cb, id) => {
+        const query = "UPDATE " + table + " SET " + condition + " WHERE id = ?";
+        connection.query(query, id, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
 }
 
 module.exports = orm;
