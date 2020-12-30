b1ef5c8c984235c9bb481bd92271f55343fa0acb
Chris Lyko <chrislyko@gmail.com>
2020-12-30 10:34:01 -0500

    server set up

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({
+  extended: false
+}));
+
+app.use(express.json());
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+app.use("/", routes);
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
